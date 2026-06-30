cb6e45896dc564bc149ea9477d452ca1fa9e9e0b
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-06-30 22:14:02 +0530

    Commit : Best_Time_to_Buy_and_Sell_Stock

---
+++ DSA/DSA/Arrays/Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class Best_Time_to_Buy_and_Sell_Stock
+    {
+        static int profit(int[] price)
+        {
+            int minprice = int.MaxValue;
+            int maxprofit = 0;
+            foreach (int p in price)
+            {
+                minprice = Math.Min(minprice, p);
+                maxprofit = Math.Max(maxprofit, p - minprice);
+            }
+            return maxprofit;
+        }
+    }
+}
