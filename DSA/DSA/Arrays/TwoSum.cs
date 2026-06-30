4f70727122578118d5cd07f0ef8ec9c894906c67
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-06-30 18:49:01 +0530

    Commit : TwoSum

---
+++ DSA/DSA/Arrays/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class TwoSum
+    {
+        static int[] profit(int[] num, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < num.Length; i++)
+            {
+                int comp = target - num[i];
+                if (map.ContainsKey(comp))
+                {
+                    return new int[] { map[comp], i };
+                }
+                map[num[i]] = i;
+            }
+            return Array.Empty<int>();
+        }
+       
+    }
+}
