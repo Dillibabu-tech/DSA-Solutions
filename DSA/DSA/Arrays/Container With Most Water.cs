a3200f5f7d413e99927f382f0e0a74aa3773ca91
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-06-30 18:06:50 +0530

    Commit : Container_With_Most_Water

---
+++ DSA/DSA/Arrays/Container With Most Water.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class Container_With_Most_Water
+    {
+        public static int Container(int[] height)
+        {
+            int min = 0;
+            int max = height.Length - 1;
+            int result = 0;
+            while (max > min)
+            {
+                result = Math.Max(max, Math.Min(height[min], height[max] - (min * max)));
+
+                if (height[max] > height[min])
+                {
+                    min++;
+                }
+                else
+                {
+                    min--;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ DSA/DSA/Arrays/Search in Rotated Sorted Array.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class Search_in_Rotated_Sorted_Array
+    {
+     public static int Sort(int[] num, int target)
+        {
+           
+            int min = 0;
+            int max = num.Length - 1;
+            while (min <= max)
+            {
+                int mid = min + (max - min) / 2;
+                if (num[mid] == target)
+                {
+                    return mid;
+                }
+                if (num[mid] >= num[min])
+                {
+                    if (target >= num[min] && target < num[mid])
+                    {
+                        max = mid - 1;
+                    }
+                    else
+                    {
+                        min = mid + 1;
+                    }
+                }
+                else
+                {
+                    if (target > num[mid] && target <= num[max])
+                    {
+                        min = mid + 1;
+                    }
+                    else
+                    {
+                        max = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
