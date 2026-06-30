ecd7b2db66b03156d8f6029d399783e5835fab22
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-06-30 22:20:24 +0530

    Commit : findduplicate1

---
+++ DSA/DSA/Arrays/findduplicate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class findduplicate
+    {
+        static bool findduplicate1(int[] num)
+        {
+            HashSet<int> seen = new HashSet<int>();
+            foreach (int n in num)
+            {
+                if (!seen.Add(n))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
