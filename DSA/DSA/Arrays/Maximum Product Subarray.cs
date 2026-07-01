885e7270f52651dbee6265f0fa56f37139cef394
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-01 10:10:33 +0530

    Commit : Maximum_Product_Subarray

---
+++ DSA/DSA/Arrays/Maximum Product Subarray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class Maximum_Product_Subarray
+    {
+        public int MaxProduct(int[] nums)
+        {
+            int max = nums[0], min = nums[0], res = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int a = nums[i], prevMax = max;
+                max = Math.Max(a, Math.Max(max * a, min * a));
+                min = Math.Min(a, Math.Min(prevMax * a, min * a));
+                res = Math.Max(res, max);
+            }
+            return res;
+        }
+    }
+}
