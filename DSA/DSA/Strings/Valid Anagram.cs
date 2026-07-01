77c54510dd1e25c6e9ed560a433922bea9819049
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-01 10:14:16 +0530

    Commit : Valid_Anagram

---
+++ DSA/DSA/Strings/Valid Anagram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Strings
+{
+    internal class Valid_Anagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+               int[] cnt = new int[26];
+            foreach (char c in s)
+            {
+                cnt[c - 'a']++;
+            }
+              foreach (char c in t)
+            {
+                if (--cnt[c - 'a'] < 0)
+                {
+                    return false;
+                }
+            }
+                                
+            return true;
+        }
+    }
+}
