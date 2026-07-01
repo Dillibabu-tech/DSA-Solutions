ed081d2349815486bc4c975a1fa835276cfe9362
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-01 12:21:48 +0530

    Commit : Valid_Palindrome

---
+++ DSA/DSA/Strings/Valid Palindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Strings
+{
+    internal class Valid_Palindrome
+    {
+        public static bool str(string str1)
+        {
+            int min = 0;
+            int max = str1.Length - 1;
+            while(max > min)
+            {
+                while(max > min && !char.IsLetterOrDigit(str1[max]));
+                min++;
+                while (max > min && !char.IsLetterOrDigit(str1[min])) ;
+                max--;
+                if(char.ToLower(str1[min]) != char.ToLower(str1[min]))
+                {
+                return false;
+                }
+                min++;
+                max--;
+            }
+            return true;
+        }
+        
+    }
+}
