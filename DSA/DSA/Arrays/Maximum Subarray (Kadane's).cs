441b0f51d4e6094e59e81144a4d698269f92a354
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-01 10:08:24 +0530

    Commit : MaxSubArray

---
+++ DSA/DSA/Arrays/Maximum Subarray (Kadane's).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class Maximum_Subarray__Kadane_s_
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSum = nums[0], cur = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                cur = Math.Max(nums[i], cur + nums[i]);
+                maxSum = Math.Max(maxSum, cur);
+            }
+            return maxSum;
+        }
+    }
+}
