d713cd14e86b2660dd4b7fc820082aac1e968df3
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-01 10:07:14 +0530

    Commit : ProductExceptSelf

---
+++ DSA/DSA/Arrays/Product of Array Except Self.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class Product_of_Array_Except_Self
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int n = nums.Length;
+            int[] res = new int[n];
+            res[0] = 1;
+            for (int i = 1; i < n; i++) 
+               res[i] = res[i - 1] * nums[i - 1];
+            int right = 1;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                res[i] *= right;
+                right *= nums[i];
+            }
+            return res;
+        }
+    }
+}
