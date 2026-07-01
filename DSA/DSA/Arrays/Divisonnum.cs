9fc01de43595b9f0aa1e6235818ec493be19b27f
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-01 09:33:12 +0530

    Commit : Divison Num

---
+++ DSA/DSA/Arrays/Divisonnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class Divisonnum
+    {
+        static int[] divis(int[] nums)
+        {
+            int total = 1;
+            foreach (int n in nums)
+            {
+                //total = n;
+                total = total * n;
+            }
+            int[] result = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = total / nums[i];
+            }
+            return result;
+        }
+    }
+}
