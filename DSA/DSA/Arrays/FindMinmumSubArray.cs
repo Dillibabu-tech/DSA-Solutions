3d1766c6c3946e8defd01f87f43736c1d48f6cff
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-01 10:00:08 +0530

    Commit : FindMinSubArray

---
+++ DSA/DSA/Arrays/FindMinmumSubArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    internal class FindMinmumSubArray
+    {
+        static int Minsub(int[] num)
+        {
+            int min = 0;
+            int max = num.Length - 1;
+            while (min < max)
+            {
+                int mid = (min + max) / 2;
+                if (num[mid] > num[max])
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid;
+                }
+            }
+            return num[max];
+        }
+    }
+}
