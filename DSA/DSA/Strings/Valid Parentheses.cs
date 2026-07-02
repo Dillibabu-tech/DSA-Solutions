0c9bc90c1495cc0ddb1b8c18f4086d5febc44fd6
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-02 14:20:37 +0530

    Commit : Valid_Parentheses

---
+++ DSA/DSA/Strings/Valid Parentheses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Strings
+{
+    internal class Valid_Parentheses
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> str = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    str.Push(c);
+                }
+                
+                else
+                {
+                    if(str.Count == 0)
+                    {
+                        return false;
+                    }
+                    char t = str.Pop();
+                    if((c == ')' && t != '(' || c == '}' && t != '{' || c == ']' && t != '[' ))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return str.Count == 0;
+        }
+    }
+}
