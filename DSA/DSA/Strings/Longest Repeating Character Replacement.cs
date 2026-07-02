f28edc56a4b3aac5db5f568e6e6817b9cc94aa40
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-02 09:49:30 +0530

    Commit : Longestcharacter

---
+++ DSA/DSA/Strings/Longest Repeating Character Replacement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Strings
+{
+    internal class Longest_Repeating_Character_Replacement
+    {
+        public static int Longestchar(string s,int n)
+        {
+            int max = 0;
+            int min = 0;
+            int res = 0;
+            int[] curr = new int[26];
+            for(int i = 0;i<s.Length;i++)
+            {
+                max = Math.Max(max, ++curr[s[i] - 'A']);
+                while((i-min+1)- max>n)
+                {
+                    curr[s[min++] - 'A']--;
+                }
+                res = Math.Max(res, i - min + 1);
+            }
+            return res;
+        }
+    }
+}
