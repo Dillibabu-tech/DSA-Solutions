2c0ce1ec816634113578da8ca209847ea5692c7d
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-03 12:50:34 +0530

    Commit : Minimum_Window_Substring

---
+++ DSA/DSA/Strings/Minimum Window Substring.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Strings
+{
+    internal class Minimum_Window_Substring
+    {
+        static string minimunWindow(string s ,string t)
+        {
+            Dictionary<char, int> need = new Dictionary<char, int>();
+            foreach (char c in t)
+            {
+                if (need.ContainsKey(c))
+                {
+                    need[c]++;
+                }
+                else
+                {
+                    need[c] = 1;
+                }
+            }
+                Dictionary<char, int> window = new Dictionary<char, int>();
+                int left = 0;
+                int formed = 0;
+                int minlength = int.MaxValue;
+                int req = need.Count;
+                int start = 0;
+                for(int i = 0;i<s.Length;i++)
+                {
+                    char cur = s[i];
+                    if(window.ContainsKey(cur))
+                    {
+                        window[cur]++;
+                    }
+                    else
+                    {
+                        window[cur] = 1;
+                    }
+                    if (need.ContainsKey(cur) && window[cur] == need[cur])
+                    {
+                        formed++;
+                    }
+                    while (formed == req)
+                    {
+                        if (i - left + 1 < minlength)
+                        {
+                            minlength = i - left + 1;
+                            start = left;
+                        }
+                        char cur1 = s[left];
+                        window[cur1]--;
+                        if(need.ContainsKey(cur1) && window[cur1] < need[cur1])
+                        {
+                            formed--;
+                        }
+                        left++;
+                    }
+
+                }            
+               if(minlength == int.MaxValue)
+                {
+                    return "";
+                }
+                return s.Substring(start, minlength);
+            }
+          
+        }
+
+    }
