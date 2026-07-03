f4e9c559c46da2c419040c322f00ebded721a942
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-03 16:17:51 +0530

    Commit : Palindromic_Substrings

---
+++ DSA/DSA/Strings/Palindromic Substrings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Strings
+{
+    internal class Palindromic_Substrings
+    {
+        public static int Palindromic(string s)
+        {
+            int count = 0;
+            for(int i =0;i<s.Length;i++)
+            {
+                count += Expand(s, i, i);
+                count += Expand(s, i, i + 1);
+            }
+            return count;
+        }
+        public static int Expand(string s,int left,int right)
+        {
+            int count = 0;
+            while (left >= 0 && right < s.Length  && s[left] == s[right])
+            {
+                count++;
+
+                left--;
+                right++;
+            }
+            return count;
+        }
+    }
+}
