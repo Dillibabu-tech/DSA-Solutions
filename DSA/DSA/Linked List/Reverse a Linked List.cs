21c1edc90d84c8b615ff4b75d8aa60a94a1390f7
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-07 12:28:37 +0530

    Commit : ReverseLinkedList

---
+++ DSA/DSA/Linked List/Reverse a Linked List.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Linked_List
+{
+    internal class Reverse_a_Linked_List
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int value)
+            {
+                val = value;
+                next = null;
+            }
+        }
+        public static ListNode ReverseList(ListNode str)
+        {
+            ListNode l = null;
+            ListNode l1 = str;
+            while(l1 != null)
+            {
+                ListNode next = l1.next;
+                l1.next = l;
+                l = l1;
+                l1 = next;   
+
+            }
+            return l;
+        }
+    }
+}
