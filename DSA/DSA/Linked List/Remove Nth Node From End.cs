59cab8345b6f4ac65c8ddf2fa4a67afc99c66203
Dillibabu-tech <reddydillibabu63@gmail.com>
2026-07-08 13:48:20 +0530

    Commit : Remove-Nth-Node

---
+++ DSA/DSA/Linked List/Remove Nth Node From End.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Linked_List
+{
+    internal class Remove_Nth_Node_From_End
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int value) 
+            { 
+                val = value;
+                next = null;
+            }
+        }
+        public class list
+        {
+            public static ListNode FindNth(ListNode ln,int n)
+            {
+                ListNode demo = new ListNode(0) { next = ln};
+                ListNode right = demo;
+                ListNode left = demo;
+                for(int i = 0;i<=n;i++)
+                {
+                    right = right.next;
+                }
+                while(right != null)
+                {
+                    right = right.next;
+                    left = left.next;
+                }
+                left.next = left.next.next;
+                return demo.next;
+            }           
+        }
+    }
+}
